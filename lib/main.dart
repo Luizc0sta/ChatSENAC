7db496b1d41bbef55f1f20d165fce86a3a1091a2
Luizc0sta <luizdenando@gmail.com>
2026-07-23 18:44:15 -0300

    Segundo commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,47 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    Center(
-      child: Text(
-        'Hello, world!',
-        textDirection: TextDirection.ltr,
-        style: TextStyle(fontSize: 30.0),
-      ),
-    ),
-  );
+  runApp(MyApp());
 }
+
+
+
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: Login(),
+      ),
+    );
+  }
+}
+
+
+
+class Login extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+      FlutterLogo(size: 18,),
+        Text('ChatSENAC'),
+        Text("Entre na sua conta"),
+        Text("Coloque seu email e senha para logar"),
+        Text("Email"),
+        TextField(),
+        InkWell(child: Text("Esqueceu a senha?")),
+        ElevatedButton(onPressed: null, child: Text("Entrar")),
+        Text("Ou"),
+        ElevatedButton(onPressed: null, child: Text("Continuar com Google")),
+        ElevatedButton(onPressed: null, child: Text("Continuar com Facebook")),
+        Text("Não tem uma conta?"),
+        InkWell(child: Text("Cadastra-se")),
+      ],
+    );
+  }
+}
+
+
+
